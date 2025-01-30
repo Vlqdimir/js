4dcc35bb21f1a4df65fc3cd02f77dc41e35a95ae
Vladimir <fedordo513@gmail.com>
2025-01-30 09:17:15 +0300

    first

---
+++ answer.js
@@ -0,0 +1,57 @@
+// const owl = {
+//     g: 'all',
+//     3: 'tur',
+//     fun: 'turews'
+// };
+// console.log(owl.g);
+
+// owl.genry = '1234';
+// console.log(owl['genry']);
+
+// const desmont = {
+//     wan: 'tret',
+//     2: 'terd',
+//     tree: 'revb',
+//     4: 'uiui'
+// };
+// console.log(desmont.wan);
+// desmont['tom'] = '1234';
+// console.log(desmont['tom']);
+
+
+
+
+// const rem = prompt('ты дурак?');
+// const answers = [];
+// answers[0] = prompt('как ваше имя?', '');
+// answers[1] = prompt('как ваша фамилия?', '');
+// answers[2] = prompt('как ваше отчество?', '');
+// answers[3] = prompt('какой год вашего рождения?', '');
+// console.log(typeof(answers));
+// const user = 'Vlad';
+// const rem = prompt(`Привет, ${user}, как у вас дела?`,'fd' );
+// document.write(rem);
+// const first = 'van';
+// const second = 'dag';
+
+// const answer = `${first}${second}`;
+// document.write(answer);
+
+
+// const time = {
+//     "1hour": '30sec',
+//     tom: '40h', 
+// };
+// console.log(time.tom);
+// time.far = '123123'
+// console.log(time['far']);
+
+
+// console.log('poc' + '-poc');
+// console.log(4 + +5 + +2);
+// console.log(7%2);
+
+const numberOfFilms = prompt('сколько фильмов вы уже посмотрели?', '');
+console.log(numberOfFilms);
+document.write(numberOfFilms)
+
